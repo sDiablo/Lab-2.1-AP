825ae60ddfc96fae6ddcbf3b23815ee71a7e7d5a
sDiablo <133573853+sDiablo@users.noreply.github.com>
2024-04-15 00:49:09 +0300

    Add files via upload

---
+++ Lab2.1.cpp
@@ -0,0 +1,29 @@
+// Lab_2.1.cpp
+// < Кав’як Назарій Іванович >
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 25
+
+#include <iostream>>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x;  // вхідний параметр
+	double z1; // результат першого виразу 
+	double z2; // результат другого виразу
+
+	cout << "x = "; cin >> x;//second change 
+	z1 = 1 - 1 / 4. * sin(2 * x) * sin(2 * x) + cos(2 * x);//перший вираз
+	z2 = cos(x) * cos(x) + cos(x) * cos(x) * cos(x) * cos(x);
+
+	cout << endl;
+
+ 
+		cout << "z1 = " << z1 << endl;//first change 
+ 
+		cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
